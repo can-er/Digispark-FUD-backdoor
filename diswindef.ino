c9a5e8ac1fdbc74828a0d10f5c2e4a03e4fd7284
can-er <47574594+can-er@users.noreply.github.com>
2021-07-04 17:30:47 +0200

    Update diswindef.ino

--- diswindef.ino
+++ diswindef.ino
@@ -1,7 +1,7 @@
 #include <DigiKeyboardFr.h>
 /*
  * Tested on:
- * AZERTY French Keyboard
+ * AZERTY Belgian French Keyboard
  * Microsoft Windows 10 Professionnel - 10.0.19042 N/A Build 19042
  * Microsoft Windows 10 Home          - 10.0.19042 N/A Build 19042
  * 
